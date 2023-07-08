7152cf586c7d5692a37ca9f6325e29cdbce08e0a
unknown <farsanarahimfr@gmail.com>
2023-07-08 17:02:28 +0530

    Pop ups

--- src/New.js
+++ src/New.js
@@ -3,16 +3,48 @@ import { Form } from 'react-bootstrap'
 import './New.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {numbers,upper,lower,chars} from './Character.js'
+import { toast,ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function New() {
 
-  const [pwd,setPwd]=useState("Min 8 characters")
+  const [pwd,setPwd]=useState('')
   const [pwdLen,setPwdLen]=useState(8)
   const [includeUpper,setIncludeUpper]=useState(false)
   const [includeLower,setIncludeLower]=useState(false)
   const [includeChar,setIncludeChar]=useState(false)
   const [includeNum,setIncludeNum]=useState(false)
   
+  const notify=(msg,error=false)=>{
+    if(error)
+    {
+      toast.error(msg, {
+        position: "top-center",
+        autoClose: 200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+    else
+    {
+      toast.info(msg, {
+        position: "top-center",
+        autoClose: 200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+  }
   const copyToClipboard=()=>{
+    if(pwd!='')
+    {
     const newTextArea=document.createElement('textarea');
     newTextArea.innerText=pwd;
      document.body.appendChild(newTextArea)
@@ -20,10 +52,17 @@ function New() {
     document.execCommand('copy')
     newTextArea.remove()
     
+      notify('Copied to Clipboard')
+    }
+    
   }
   const generatePwd=(e)=>{
     e.preventDefault()
     let characterList=''
+    if(!(includeChar||includeLower||includeNum||includeUpper))
+    {
+      notify('Please choose any options',true)
+    }
     if(includeLower)
     {
       characterList+=lower
@@ -54,7 +93,7 @@ function New() {
     return pwd;
   }
   return (
-    <div className='form' >
+    <div className='form mt-5' >
       <div className='main container w-75 p-3'>
       
         <h1 className='text-light text-center'><i class="fa-solid fa-lock"></i>Password Generator</h1>
@@ -116,11 +155,25 @@ function New() {
             />
           </div>
           <button onClick={generatePwd} type="submit" className='mt-2 btn btn-success w-100 rounded-3'>Generate Password</button>
+          
         </Form.Group>
           </Form>
           
       </div>
+      <ToastContainer
+position="top-center"
+autoClose={100}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
     </div>
+
   )
 }
 
